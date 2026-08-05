2d5f64a04b57bb82705413bdba58abf2a4d4be92
Jared Davies <jardavies89@gmail.com>
2026-08-05 16:24:57 -0500

    fixed migration (#37)

--- api/drizzle/0001_powerful_ozymandias.sql
+++ api/drizzle/0001_powerful_ozymandias.sql
@@ -1,2 +1,2 @@
-UPDATE entries SET medium = 'SHOW', metadata = '{"style":"ANIME"}'::jsonb WHERE medium = 'ANIME';
 ALTER TABLE "entries" ADD COLUMN "metadata" jsonb;
+UPDATE entries SET medium = 'SHOW', metadata = '{"style":"ANIME"}'::jsonb WHERE medium = 'ANIME';
